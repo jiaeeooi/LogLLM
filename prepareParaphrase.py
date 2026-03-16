47ca883b5c41a72060103871ad41fd0a0f017c07
jiaeeooi <ooijiaee@gmail.com>
2026-03-16 05:37:00 +0000

    Update Paraphrasing

--- prepareParaphrase.py
+++ prepareParaphrase.py
@@ -90,8 +90,9 @@ for i in tqdm(range(0, len(all_logs), batch_size)):
         **encoding,
         max_length=64,
         do_sample=True,
-        top_k=120,
-        top_p=0.95
+        temperature=1.7,
+        top_k=50,
+        top_p=0.85
     )
 
     decoded = tokenizer.batch_decode(outputs, skip_special_tokens=True)

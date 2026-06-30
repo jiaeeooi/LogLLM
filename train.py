fd12aded42b37f50997069f78bc5ae29fe212650
jiaeeooi <ooijiaee@gmail.com>
2026-06-30 22:39:11 +0000

    Update

--- train.py
+++ train.py
@@ -31,13 +31,14 @@ data_path = "/content/drive/MyDrive/LogLLM/BGL/bgl_train.csv"
 
 min_less_portion = 0.3
 
-Bert_path = "bert-base-uncased"
+#Bert_path = "bert-base-uncased"
+Bert_path = "sentence-transformers/all-mpnet-base-v2"
 Llama_path = "meta-llama/Meta-Llama-3-8B"
 
 ROOT_DIR = Path(__file__).parent
 #ft_path = os.path.join(ROOT_DIR, r"ft_model_{}".format(dataset_name))
 #ft_path = f"/content/drive/MyDrive/LogLLM/results/ft_model_{dataset_name}"
-ft_path = f"/content/drive/MyDrive/LogLLM/results/ft_model_{dataset_name}_meanpool"
+ft_path = f"/content/drive/MyDrive/LogLLM/results/ft_model_{dataset_name}_mpnet"
 
 device = torch.device("cuda:0")
 

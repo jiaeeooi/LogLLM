9a9e0e5cbcd5fb0718ce0100420231d0037127c5
jiaeeooi <ooijiaee@gmail.com>
2026-09-19 00:19:50 +0000

    Update

--- train.py
+++ train.py
@@ -44,7 +44,6 @@ Llama_path = "meta-llama/Meta-Llama-3-8B"
 ROOT_DIR = Path(__file__).parent
 #ft_path = os.path.join(ROOT_DIR, r"ft_model_{}".format(dataset_name))
 #ft_path = f"/content/drive/MyDrive/LogLLM/results/ft_model_{dataset_name}"
-ft_path = f"/content/drive/MyDrive/LogLLM/results/ft_model_{dataset_name}_2"
 #ft_path = f"/content/drive/MyDrive/LogLLM/results/ft_model_{dataset_name}_meanpool"
 #ft_path = f"/content/drive/MyDrive/LogLLM/results/ft_model_{dataset_name}_mpnet"
 #ft_path = f"/content/drive/MyDrive/LogLLM/results/ft_model_{dataset_name}_mpnet_finetune"

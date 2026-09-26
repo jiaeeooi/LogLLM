556dc66699ea2e42480c3f8501f88c0129448fa3
jiaeeooi <ooijiaee@gmail.com>
2026-09-26 00:47:15 +0000

    Update

--- run_umap.py
+++ run_umap.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 import umap
 
 #UMAP_OUTPUT = "/content/drive/MyDrive/LogLLM/results/embeddings_BGL/umap_author.csv"
-PLOT_OUTPUT = "/content/drive/MyDrive/LogLLM/results/embeddings_BGL/umap_sbert.png"
+PLOT_OUTPUT = "/content/drive/MyDrive/LogLLM/results/embeddings_BGL/umap_bge.png"
 
 # Paths
-EMBEDDINGS_PATH = "/content/drive/MyDrive/LogLLM/results/embeddings_BGL/embeddings_sbert.npy"
-METADATA_PATH = "/content/drive/MyDrive/LogLLM/results/embeddings_BGL/embedding_metadata_sbert.csv"
+EMBEDDINGS_PATH = "/content/drive/MyDrive/LogLLM/results/embeddings_BGL/embeddings_bge.npy"
+METADATA_PATH = "/content/drive/MyDrive/LogLLM/results/embeddings_BGL/embedding_metadata_bge.csv"
 #PREDICTIONS_PATH = "/content/drive/MyDrive/LogLLM/BGL/test_preds_original.csv"
 
 # Load
